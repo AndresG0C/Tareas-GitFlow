38be270363d4bd758fcc6a3e45e486d6107323c9
AndresG0C <andresgonzalez22004@gmail.com>
2026-04-27 12:01:29 -0500

    feat(CRUD): Implementación de la operación eliminar tareas

--- backend-node/src/crud/taskCrud.ts
+++ backend-node/src/crud/taskCrud.ts
@@ -26,3 +26,12 @@ export const getTasks = async (): Promise<Task[]> => {
 export const getTaskById = async (taskId: number): Promise<Task | null> => {
     return await taskRepository.findOneBy({ id: taskId });
 };
+
+// DELETE: Eliminar una tarea
+export const deleteTask = async (taskId: number): Promise<boolean> => {
+    const task = await taskRepository.findOneBy({ id: taskId });
+    if (!task) return false;
+
+    await taskRepository.remove(task);
+    return true;
+};
