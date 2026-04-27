3bd3292c9c846abfd8afe9e3cf1133686a10f003
AndresG0C <andresgonzalez22004@gmail.com>
2026-04-27 12:00:31 -0500

    feat(CRUD): Implementación de la operación obtener tareas

--- backend-node/src/crud/taskCrud.ts
+++ backend-node/src/crud/taskCrud.ts
@@ -6,12 +6,23 @@ const taskRepository = AppDataSource.getRepository(Task);
 
 // CREATE: Crear una tarea
 export const createTask = async (taskData: CreateTaskDto): Promise<Task> => {
-  const dbTask = taskRepository.create({
-    title: taskData.title,
-    completed: false
-  });
-  
-  await taskRepository.save(dbTask);
-  return dbTask;
+    const dbTask = taskRepository.create({
+        title: taskData.title,
+        completed: false
+    });
+
+    await taskRepository.save(dbTask);
+    return dbTask;
 };
 
+// READ ALL: Obtener todas las tareas
+export const getTasks = async (): Promise<Task[]> => {
+    return await taskRepository.find({
+        order: { id: "ASC" }
+    });
+};
+
+// READ ONE: Obtener una tarea por ID
+export const getTaskById = async (taskId: number): Promise<Task | null> => {
+    return await taskRepository.findOneBy({ id: taskId });
+};
